4fc3a4e5b5afe52ae6f5272b50cb2ecb863bec79
someone-1 <iam.nageshbs@gmail.com>
2017-02-05 01:35:25 +0530

    blocking operation

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var server = require("./server");
+var router = require('./router');
+var requesthandler = require('./requesthandler');
 
-server.start();
+var handler = {};
+
+handler['/'] = requesthandler.start;
+handler['/start'] = requesthandler.start;
+handler['/upload'] = requesthandler.upload;
+
+server.start(router.route , handler);
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-var http = require('http');
+var http = require("http");
+var url = require("url");
 
-start = () =>{
-	onRequest = (request , response) => {
-		console.log("request recieved.");
-		response.writeHead(200, {"Content-Type":"text/plain"});
-		response.write("hello world");
+function start(route, handle) {
+	function onRequest(request, response) {
+
+		//  http://localhost:8888/start?foo=bar&hello=world
+		//  /start is the pathname of the above string url.parse(string).pathname
+		//  query string is the part that comes after ? eg :-foo=bar&hello=world 
+		//  it is seperated by & . url.parse(string).query
+
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		
+		var content = route(handle, pathname)
+		response.write(content);
 		response.end();
 	}
 	http.createServer(onRequest).listen(8080);
-	console.log("server has started");
+	console.log("Server has started.");
 }
 exports.start = start;
