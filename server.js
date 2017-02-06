cb4731ff2733f229d6a62beb0e86657b549b1638
someone-1 <iam.nageshbs@gmail.com>
2017-02-06 22:16:42 +0530

    handling post request

--- server.js
+++ server.js
@@ -11,12 +11,17 @@ function start(route, handle) {
 
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
-		
+		var postData = '';
+		request.setEncoding("utf8");
+		request.addListener("data", function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '"+ postDataChunk + "'.");
+		});
 
 		// sending response object as parameter ->router -> requesthandlers
-		// 
-		route(handle, pathname , response)
-		
+		request.addListener("end", function() {
+			route(handle, pathname, response, postData);
+		});
 	}
 	http.createServer(onRequest).listen(8080);
 	console.log("Server has started.");
